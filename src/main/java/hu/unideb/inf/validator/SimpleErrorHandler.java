9a29a24d1f507accb99e27c07e6cc9fb35ec57ae
Péter Jeszenszky <vackor2@gmail.com>
2019-09-23 09:04:22 +0200

    Reformatted source code.

--- src/main/java/hu/unideb/inf/validator/SimpleErrorHandler.java
+++ src/main/java/hu/unideb/inf/validator/SimpleErrorHandler.java
@@ -9,38 +9,38 @@ import org.xml.sax.ErrorHandler;
 
 public class SimpleErrorHandler implements ErrorHandler {
 
-	private PrintWriter out;
-	private boolean failOnError = false;
+    private PrintWriter out;
+    private boolean failOnError = false;
 
-	public SimpleErrorHandler() {
-		out = new PrintWriter(System.err, true);
-	}
+    public SimpleErrorHandler() {
+        out = new PrintWriter(System.err, true);
+    }
 
-	public SimpleErrorHandler(boolean failOnError) {
-		this();
-		this.failOnError = failOnError;
-	}
+    public SimpleErrorHandler(boolean failOnError) {
+        this();
+        this.failOnError = failOnError;
+    }
 
-	public SimpleErrorHandler(PrintWriter out) {
-		this.out = out;
-	}
+    public SimpleErrorHandler(PrintWriter out) {
+        this.out = out;
+    }
 
-	public void warning(SAXParseException e) throws SAXException {
-		printException("Warning", e);
-	}
+    public void warning(SAXParseException e) throws SAXException {
+        printException("Warning", e);
+    }
 
-	public void error(SAXParseException e) throws SAXException {
-		printException("Error", e);
-		if (failOnError) throw e;
-	}
+    public void error(SAXParseException e) throws SAXException {
+        printException("Error", e);
+        if (failOnError) throw e;
+    }
 
-	public void fatalError(SAXParseException e) throws SAXException {
-		printException("Fatal error", e);
-		throw e;
-	}
+    public void fatalError(SAXParseException e) throws SAXException {
+        printException("Fatal error", e);
+        throw e;
+    }
 
-	private void printException(String prefix, SAXParseException e) throws SAXException {
-		out.format("[%s] %s:%d:%d: %s%n", prefix, e.getSystemId(), e.getLineNumber(), e.getColumnNumber(), e.getMessage());
-	}
+    private void printException(String prefix, SAXParseException e) throws SAXException {
+        out.format("[%s] %s:%d:%d: %s%n", prefix, e.getSystemId(), e.getLineNumber(), e.getColumnNumber(), e.getMessage());
+    }
 
 }
--- src/main/java/hu/unideb/inf/validator/XSD11Validator.java
+++ src/main/java/hu/unideb/inf/validator/XSD11Validator.java
@@ -10,28 +10,28 @@ import org.xml.sax.SAXParseException;
 
 public class XSD11Validator {
 
-	public static void main(String[] args) {
-		if (args.length != 1 && args.length != 2) {
-			System.err.printf("java %s [schema] instance%n", XSD11Validator.class.getName());
-			System.exit(1);
-		}
-		System.setProperty("javax.xml.validation.SchemaFactory:http://www.w3.org/XML/XMLSchema/v1.1",
-			"org.apache.xerces.jaxp.validation.XMLSchema11Factory");		
-		try {
-			new org.apache.xerces.jaxp.validation.XMLSchema11Factory();
-			SchemaFactory sf = SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1");
-			sf.setErrorHandler(new SimpleErrorHandler(true));
+    public static void main(String[] args) {
+        if (args.length != 1 && args.length != 2) {
+            System.err.printf("java %s [schema] instance%n", XSD11Validator.class.getName());
+            System.exit(1);
+        }
+        System.setProperty("javax.xml.validation.SchemaFactory:http://www.w3.org/XML/XMLSchema/v1.1",
+                "org.apache.xerces.jaxp.validation.XMLSchema11Factory");
+        try {
+            new org.apache.xerces.jaxp.validation.XMLSchema11Factory();
+            SchemaFactory sf = SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1");
+            sf.setErrorHandler(new SimpleErrorHandler(true));
 
-			Schema schema = args.length == 1 ? sf.newSchema() : sf.newSchema(new StreamSource(args[0]));
-			StreamSource instance = new StreamSource(args.length == 1 ? args[0] : args[1]);
+            Schema schema = args.length == 1 ? sf.newSchema() : sf.newSchema(new StreamSource(args[0]));
+            StreamSource instance = new StreamSource(args.length == 1 ? args[0] : args[1]);
 
-			Validator validator = schema.newValidator();
-			validator.setErrorHandler(new SimpleErrorHandler(false));
-			validator.validate(instance);
-		} catch(SAXParseException e) {
-		} catch(Exception e) {
-			System.err.println(e.getMessage());
-		}
-	}
+            Validator validator = schema.newValidator();
+            validator.setErrorHandler(new SimpleErrorHandler(false));
+            validator.validate(instance);
+        } catch (SAXParseException e) {
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 
 }
